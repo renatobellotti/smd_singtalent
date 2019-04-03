17c092e40b0949641d6809690b9510ca93b12514
Renato Bellotti <renato-jun.bellotti@bluewin.ch>
2019-04-03 12:02:22 +0200

    Added Python script to generate the hashes.

---
+++ generate_hashes.py
@@ -0,0 +1,32 @@
+'''Generates hashes in a format suitable for the voting tool.'''
+import random
+import string
+
+# number of unique hashes to be generated
+N = 1000
+# number of characters per hash
+numCharPerHash = 4
+outputFileName = 'output.sql'
+
+# code taken from:
+# https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits?rq=1
+hashes = set([])
+while len(hashes) < N:
+    digits = random.choices(string.ascii_uppercase + string.digits, k=numCharPerHash)
+    candidate = ''.join(digits)
+    hashes.add(candidate)
+
+# output to terminal
+for h in hashes:
+    print(h)
+
+# SQL command:
+# INSERT INTO [TABLE] ([COLUMN_NAME]) VALUES (VAL1), (VAL2), (VAL3)
+strings = ["('{}')".format(h) for h in hashes]
+valueString = ',\n'.join(strings)
+
+sql = 'INSERT INTO hashes (hash) VALUES {};'.format(valueString)
+
+# output to file
+with open(outputFileName, 'w') as file:
+    file.write(sql)
