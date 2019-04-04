b00c97ea9c69a242d9a1e35728aac5ff0aa45520
Renato Bellotti <renato-jun.bellotti@bluewin.ch>
2019-04-04 18:26:02 +0200

    Extended install script to create a table for the email addresses.

--- install.php
+++ install.php
@@ -3,4 +3,6 @@
     
     $sql = "CREATE TABLE `smd_singtalent`.`hashes` ( `hash` VARCHAR(10) NOT NULL , `first` INT NULL , `second` INT NULL , `third` INT NULL, PRIMARY KEY (`hash`) )";
     $conn->query($sql);
+    $sql = "CREATE TABLE `smd_singtalent`.`emails` ( `email_address` VARCHAR(50) NOT NULL , PRIMARY KEY (`email_address`) );";
+    $conn->query($sql);
 ?>
