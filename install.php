80e688b52dd00843b46acff1e633b256dbc1c604
Renato Bellotti <renato-jun.bellotti@bluewin.ch>
2019-04-24 23:29:12 +0200

    Bug fix in install script.

--- install.php
+++ install.php
@@ -1,8 +1,8 @@
 <?php
     require('helpers.php');
     
-    $sql = "CREATE TABLE `smd_singtalent`.`hashes` ( `hash` VARCHAR(10) NOT NULL , `first` INT NULL , `second` INT NULL , `third` INT NULL, PRIMARY KEY (`hash`) )";
+    $sql = "CREATE TABLE `$dbname`.`hashes` ( `hash` VARCHAR(10) NOT NULL , `first` INT NULL , `second` INT NULL , `third` INT NULL, PRIMARY KEY (`hash`) )";
     $conn->query($sql);
-    $sql = "CREATE TABLE `smd_singtalent`.`emails` ( `email_address` VARCHAR(50) NOT NULL , PRIMARY KEY (`email_address`) );";
+    $sql = "CREATE TABLE `$dbname`.`emails` ( `email_address` VARCHAR(50) NOT NULL , PRIMARY KEY (`email_address`) );";
     $conn->query($sql);
 ?>
