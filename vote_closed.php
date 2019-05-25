0fd22d35462fd7fa63d0cb99d3154f3d700e28fa
Renato Bellotti <renato-jun.bellotti@bluewin.ch>
2019-05-25 17:07:18 +0200

    Added vote closed page.

---
+++ vote_closed.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="de">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dietiker Singtalent</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/landing-page.css" rel="stylesheet">
+    <link href="css/form.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+  
+  <!-- Add this only to the index.php and header.php page -->
+  <style>
+      header.masthead {
+        height: 100%;
+      }
+  </style>
+
+    <!-- Masthead -->
+    <header class="masthead text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <div class="text-center"><h1 class="mb-5">Dietiker Singtalent</h1></div>
+          </div>
+          <div class="col-md-12 col-lg-12 col-xl-12 mx-auto">
+            <div class="text-center"><h1 class="mb-5">Abstimmung geschlossen</h1></div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
